f93bff4f6cbd1dadb85552b15b39ef81ab2b8383
felitommasi <felipe.tommasi@gmail.com>
2022-03-20 18:53:42 -0300

    entrega opcional 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//comprobacion de edad para acceso 
-
-let nombre = prompt('ingresa tu nombre');
+//comprobacion de edad para acceso
+//algoritmo condicional 
+/* let nombre = prompt('ingresa tu nombre');
 let edad = prompt('ingresa tu edad');
 edad = parseInt(edad);
 let confirmacion = prompt('¿confirmas que sos mayor de 18? si/no');
@@ -14,4 +14,13 @@ if ((edad >= 18) && ((confirmacion == 'si') || (confirmacion == 'SI'))){
 } 
 else{
     alert('ERROR');
+} */
+
+//Desafio - crear un algoritmo usando un ciclo
+
+let numero = parseInt(prompt('ingrese un numero para ver tabla de multiplicacion en la consola'));
+
+for (let i = 0; i <= 10; i++){
+    let resultado = numero * i;
+    console.log(numero + " x " + i + " = " + resultado);
 }
