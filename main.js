a67c3dd5fb2deb46e458fbf602798769fe424472
felitommasi <felipe.tommasi@gmail.com>
2022-04-04 20:41:38 -0300

    desafio clase 6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//valor pasaje por seccion de destino
+/* //valor pasaje por seccion de destino
 const seccionUno = 50; 
 const seccionDos = 70;
 const seccionTres = 90; 
@@ -29,3 +29,53 @@ if (saldoRestante < 0){
 }else if (saldoRestante >= 0){
     alert(`Buen viaje \n\n * Su saldo restante es: $${saldoRestante}`);
 }
+ */
+
+// ! desafio clase 6
+
+class Pasaje {
+    constructor(seccion, estacion, valor){
+        this.seccion = seccion;
+        this.estacion = estacion;
+        this.valor = valor;
+    }
+}
+const destino = [];
+
+// ? seccion 1
+destino.push(new Pasaje('seccion1', 'Yrigoyen', 50));
+destino.push(new Pasaje('seccion1', 'Avellaneda', 50));
+destino.push(new Pasaje('seccion1', 'Gerli', 50));
+destino.push(new Pasaje('seccion1', 'Lanus', 50));
+destino.push(new Pasaje('seccion1', 'Escalada', 50));
+
+// ? seccion 2
+destino.push(new Pasaje('seccion2', 'Banfield', 70));
+destino.push(new Pasaje('seccion2', 'Lomas de zamora', 70));
+destino.push(new Pasaje('seccion2', 'Temperley', 70));
+destino.push(new Pasaje('seccion2', 'Adrogue', 70));
+destino.push(new Pasaje('seccion2', 'Burzaco', 70));
+destino.push(new Pasaje('seccion2', 'Longchamps', 70));
+
+// ? seccion 3
+destino.push(new Pasaje('seccion3', 'Glew', 90));
+destino.push(new Pasaje('seccion3', 'Guernica', 90));
+destino.push(new Pasaje('seccion3', 'Korn', 90));
+
+
+//saldo actual SUBE
+let saldoActual = 80;
+
+
+let compra = prompt(`* Su saldo actual es: $80 \n\n Ingrese su destino para comprar pasaje:`);
+
+for (const precio of destino) {
+    if(precio.valor > saldoActual){
+        alert('Tu saldo es insuficiente');
+    }else{
+        alert('Buen viaje \n\n * Su saldo restante es: $ ' + (saldoActual-precio.valor))
+    }
+}
+
+
+
